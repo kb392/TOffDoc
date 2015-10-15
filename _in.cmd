126aef4eb07840cdeba2ba1ad33ed5f71f62986e
kb392 <tema@mail.ru>
2015-10-15 13:24:36 +0300

    fix file name on terminal, add AutoOpen flag

---
+++ _in.cmd
@@ -0,0 +1,4 @@
+call .setpath.cmd
+copy  %MACPATH%\Service\TOffDoc.mac .
+copy  %MACPATH%\Service\constxml.mac .
+copy  %MACPATH%\Test\test_odt.mac .
---
+++ _out.cmd
@@ -0,0 +1,4 @@
+call .setpath.cmd
+copy  TOffDoc.mac %MACPATH%\Service\
+copy  constxml.mac %MACPATH%\Service\
+copy  test_odt.mac %MACPATH%\Test\
