dcbfa3a45676fcafb4d7d337889a981666ec4830
kb392 <tema@mail.ru>
2015-11-22 20:19:00 +0300

    add sample

--- _out.cmd
+++ _out.cmd
@@ -1,4 +1,6 @@
 call .setpath.cmd
-iconv -t cp866 -f utf-8 TOffDoc.mac >%MACPATH%\Service\TOffDoc.mac
-iconv -t cp866 -f utf-8 constxml.mac >%MACPATH%\Service\constxml.mac
+iconv -t cp866 -f utf-8 TOffDoc.mac >%MACPATH%\Lib\TOffDoc.mac
+iconv -t cp866 -f utf-8 constxml.mac >%MACPATH%\Lib\constxml.mac
 iconv -t cp866 -f utf-8 test_odt.mac >%MACPATH%\Test\test_odt.mac
+iconv -t cp866 -f utf-8 test_odt_2.mac >%MACPATH%\Test\test_odt_2.mac
+iconv -t cp866 -f utf-8 termtools.mac >%MACPATH%\.TERMINAL\termtools.mac
