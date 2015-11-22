36d9825bace50ce05d18c03fd117242871fe5594
kb392 <tema@mail.ru>
2015-11-22 16:16:53 +0300

    change repo to utf-8

--- _in.cmd
+++ _in.cmd
@@ -1,4 +1,4 @@
 call .setpath.cmd
-copy  %MACPATH%\Service\TOffDoc.mac .
-copy  %MACPATH%\Service\constxml.mac .
-copy  %MACPATH%\Test\test_odt.mac .
+iconv -f cp866 -t utf-8 %MACPATH%\Service\TOffDoc.mac >.\TOffDoc.mac
+iconv -f cp866 -t utf-8 %MACPATH%\Service\constxml.mac >.\constxml.mac
+iconv -f cp866 -t utf-8 %MACPATH%\Test\test_odt.mac >.\test_odt.mac
--- _out.cmd
+++ _out.cmd
@@ -1,4 +1,4 @@
 call .setpath.cmd
-copy  TOffDoc.mac %MACPATH%\Service\
-copy  constxml.mac %MACPATH%\Service\
-copy  test_odt.mac %MACPATH%\Test\
+iconv -t cp866 -f utf-8 TOffDoc.mac >%MACPATH%\Service\TOffDoc.mac
+iconv -t cp866 -f utf-8 constxml.mac >%MACPATH%\Service\constxml.mac
+iconv -t cp866 -f utf-8 test_odt.mac >%MACPATH%\Test\test_odt.mac
