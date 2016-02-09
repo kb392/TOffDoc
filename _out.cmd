d4b42a185788004a2bb6af122d1706084313882b
kb392 <tema@mail.ru>
2016-02-09 17:44:26 +0300

    test git encoding

--- _out.cmd
+++ _out.cmd
@@ -1,6 +1,6 @@
 call .setpath.cmd
-iconv -t cp866 -f utf-8 TOffDoc.mac >%MACPATH%\Lib\TOffDoc.mac
-iconv -t cp866 -f utf-8 constxml.mac >%MACPATH%\Lib\constxml.mac
-iconv -t cp866 -f utf-8 test_odt.mac >%MACPATH%\Test\test_odt.mac
-iconv -t cp866 -f utf-8 test_odt_2.mac >%MACPATH%\Test\test_odt_2.mac
-iconv -t cp866 -f utf-8 termtools.mac >%MACPATH%\.TERMINAL\termtools.mac
+copy  TOffDoc.mac %MACPATH%\Lib\TOffDoc.mac
+copy  constxml.mac %MACPATH%\Lib\constxml.mac
+copy  test_odt.mac %MACPATH%\Test\test_odt.mac
+copy  test_odt_2.mac %MACPATH%\Test\test_odt_2.mac
+copy  termtools.mac %MACPATH%\.TERMINAL\termtools.mac
